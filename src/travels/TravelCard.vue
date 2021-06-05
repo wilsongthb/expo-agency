807abb906df664421d762877e32f372678b83e58
wil <wilsonaux1@gmail.com>
2021-06-05 15:48:33 -0500

    agregados travels

---
+++ src/travels/TravelCard.vue
@@ -0,0 +1,54 @@
+<!-- Author: Wilson Pilco Nunez -->
+<!-- Email: wilsonaux1@gmail.com -->
+<!-- Created at: 2021-06-05 14:23 -->
+<template>
+  <q-card class="my-card">
+    <img :src="travel.imageSrc" />
+
+    <q-card-section>
+      <div class="text-subtitle2">{{ travel.name }}</div>
+      <!-- <div class="text-h6">{{ travel.name }}</div> -->
+      <!-- <div class="text-subtitle2">by John Doe</div> -->
+    </q-card-section>
+
+    <!-- <q-card-section class="q-pt-none"> -->
+    <!--   {{ "nani " }} -->
+    <!-- </q-card-section> -->
+  </q-card>
+</template>
+<script>
+export default {
+  components: {
+    //
+  },
+
+  // directives
+  // filters
+
+  props: {
+    travel: {
+      required: true,
+    },
+  },
+
+  data: () => ({
+    //
+  }),
+
+  computed: {
+    //
+  },
+
+  // watch: {},
+
+  mounted() {
+    //
+  },
+
+  methods: {
+    //
+  },
+};
+</script>
+
+<style scoped></style>
