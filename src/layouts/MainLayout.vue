e09126cf07f5b020b298c4fc21aee2209a3b2dcf
c413bxd <caleb.895.9@gmail.com>
2021-06-07 22:29:31 -0500

    travel informacion

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -97,50 +97,7 @@
 <script>
 import EssentialLink from 'components/EssentialLink.vue'
 
-const linksData = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-];
+const linksData = [];
 
 export default {
   name: 'MainLayout',
@@ -151,10 +108,10 @@ export default {
       drawer: false,
       essentialLinks: linksData,
       links1: [
-        { icon: 'home', text: 'Home', url:"home"  },
-        { icon: 'luggage', text: 'Tours', url:"tours" },
-        { icon: 'search', text: 'Explore', url:"travels" },
-        { icon: 'account_box', text: 'Profile', url:"auth" }
+        { icon: 'home', text: 'Home', url:"/home"  },
+        { icon: 'luggage', text: 'Tours', url:"/tours" },
+        { icon: 'search', text: 'Explore', url:"/travels" },
+        { icon: 'account_box', text: 'Profile', url:"/auth" }
       ]
     }
   }
